b313109e4040b6c8b0ccc7f4f8961953bbc1d903
Oskar Jakieła <oskar.jakiela@allegrogroup.com>
2018-03-13 13:30:57 +0100

    Fix tests

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -1,18 +1,11 @@
-// For authoring Nightwatch tests, see
-// http://nightwatchjs.org/guide#usage
-
 module.exports = {
   'default e2e tests': function test(browser) {
-    // automatically uses dev Server port from /config.index.js
-    // default: http://localhost:8080
-    // see nightwatch.conf.js
     const devServer = browser.globals.devServerURL;
 
     browser
       .url(devServer)
-      .waitForElementVisible('#App', 5000)
-      .assert.elementPresent('.Home')
-      .assert.containsText('h1', 'Home')
+      .waitForElementVisible('.App', 5000)
+      .assert.urlEquals(`${devServer}/contacts`)
       .end();
   },
 };
--- test/unit/specs/ContactsList.spec.js
+++ test/unit/specs/ContactsList.spec.js
@@ -2,11 +2,46 @@ import Vue from 'vue';
 import ContactsList from '@/components/contacts/List';
 
 describe('ContactsList.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(ContactsList);
-    const vm = new Constructor().$mount();
+  describe('with empty contacts', () => {
+    let vm;
 
-    expect(vm.$el.querySelector('.ContactsList h2').textContent)
-      .toEqual('Contacts');
+    beforeEach(() => {
+      const Constructor = Vue.extend(ContactsList);
+
+      vm = new Constructor({
+        propsData: {
+          contacts: [],
+        },
+      }).$mount();
+    });
+
+    it('should render correctly', () => {
+      expect(vm.$el.querySelectorAll('.ContactsList__list').length).toBe(1);
+      expect(vm.$el.querySelectorAll('.ContactsListGroupHeader').length).toBe(0);
+      expect(vm.$el.querySelectorAll('.ContactsListItem').length).toBe(0);
+    });
+  });
+
+  describe('with some contacts', () => {
+    let vm;
+
+    beforeEach(() => {
+      const Constructor = Vue.extend(ContactsList);
+
+      vm = new Constructor({
+        propsData: {
+          contacts: [{
+            id: '1',
+            name: 'Foo Bar',
+          }],
+        },
+      }).$mount();
+    });
+
+    it('should render correctly', () => {
+      expect(vm.$el.querySelectorAll('.ContactsList__list').length).toBe(1);
+      expect(vm.$el.querySelector('.ContactsListGroupHeader').textContent.trim()).toBe('B');
+      expect(vm.$el.querySelector('.ContactsListItem').textContent.trim()).toBe('Bar Foo');
+    });
   });
 });
